f7e37e435e15c0d6583f22d608a7e38062e03b17
ansnoussi <anis0snoussi@gmail.com>
2018-12-13 17:32:43 +0000

    testing_class

---
+++ Ouvrage.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Ouvrage.h"
+#include <cstring>
+using namespace std;
+
+    Ouvrage::Ouvrage(){
+    }
+
+    Ouvrage::~Ouvrage(){
+        cout << "Object Ouvrage deleted !" << endl;
+    }
+}
---
+++ Ouvrage.h
@@ -0,0 +1,17 @@
+
+#ifndef OUVRAGE_H
+#define OUVRAGE_H
+using namespace std;
+
+class Ouvrage{
+private:
+    string titre;
+    string dateCreation;
+    bool existe;
+public:
+    Ouvrage();
+    ~Ouvrage();
+};
+
+
+#endif /* OUVRAGE_H */
